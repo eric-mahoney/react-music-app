0cf884248fc9b08ccbbb4c74b67bd6929616c320
ericmahoney <eric.mahoney22@gmail.com>
2021-01-10 13:48:01 -0500

    trying to fix padding issues

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = ({ setLibraryOpen, libraryOpen }) => {
   return (
     <div className="navbar-wrapper">
       <nav className="navbar">
-        <h1 className="logo">Mahoney Music App</h1>
+        <h1 className="logo">Music App</h1>
         <button className="library-btn" onClick={() => setLibraryOpen(!libraryOpen)}>
           Library
         </button>
