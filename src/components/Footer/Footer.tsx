e245c23a1525f2a480fa19948a4aa5fca6999728
ericmahoney <eric.mahoney22@gmail.com>
2021-07-21 23:21:54 -0400

    moved footer code to its own component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <a href="https://github.com/eric-mahoney/react-music-app" target="_blank" rel="noreferrer">
+        view on github
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
