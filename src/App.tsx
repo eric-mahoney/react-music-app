519285ee346ebe8ba1ae7919c696d960d8244b98
ericmahoney <eric.mahoney22@gmail.com>
2021-07-21 23:24:58 -0400

    changed app component to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Library from "./components/Library/Library";
+import Navbar from "./components/Navbar/Navbar";
+import Song from "./components/Song/Song";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <Navbar />
+      <Library />
+      <Song />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
