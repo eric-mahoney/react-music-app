28b723ae6ecff4b687f026c135f8646d9ed4a350
Eric Mahoney <eric.mahoney22@gmail.com>
2021-09-19 00:59:15 -0400

    created recoil stores for storing the state of the store and selectors for deriving the state

--- src/store/Song.ts
+++ src/store/Song.ts
@@ -1,12 +1,20 @@
 import { atom, selector } from "recoil";
+
+import { songs } from "../data";
+import { Song } from "../models";
 import { librarySongState } from "./Library";
 
-export const currentSongState = selector({
-  key: "currentSongState",
-  get: ({ get }) => get(librarySongState).filter((song) => song.active === true),
-});
-
-export const songPlayingState = atom({
+export const songPlayingState = atom<boolean>({
   key: "songPlayingState",
   default: false,
+});
+
+export const currentSongState = atom<Song>({
+  key: "currentSongState",
+  default: songs[0],
+});
+
+export const getCurrentStream = selector({
+  key: "getCurrentStream",
+  get: ({ get }) => get(currentSongState).stream,
 });
